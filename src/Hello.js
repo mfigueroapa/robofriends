96d4165acd3ce8f0cb0efbfdb5a9234e6df35edf
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-03-19 21:05:15 -0600

    Hello Component

---
+++ src/Hello.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Hello.css';
+
+const Hello = (props) => {
+    return(
+            <div className = 'f1 tc'>
+            <h1> Hello Worldd </h1>
+            <p>{props.greeting}</p>
+            <p>{props.dog}</p>
+            </div>
+    );
+    
+}
+
+export default Hello;
+
+
+// class Hello extends React.Component {
+//   render() {
+//     return (
+//       <div className="f1 tc">
+//         <h1> Hello World </h1>
+//         <p>{this.props.greeting}</p>
+//       </div>
+//     );
+//   }
+// }
+
+///////
+
+// const Hello = props => {
+//   return (
+//     <div className="f1 tc">
+//       <h1> Hello Worldd </h1>
+//       <p>{props.greeting}</p>
+//     </div>
+//   );
+// };
